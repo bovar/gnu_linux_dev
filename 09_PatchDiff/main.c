f3b46261f5cb1a59d27aaf7e4d3e67729811231f
bovar <vova.dubkov.04@mail.ru>
2025-11-22 11:28:45 +0300

    Refuctored

--- 09_PatchDiff/main.c
+++ 09_PatchDiff/main.c
@@ -5,8 +5,6 @@
 #define SIZE 6
 #define MAZE_SIZE (SIZE * 2 + 1)
 
-char maze[MAZE_SIZE][MAZE_SIZE];
-
 void print_mat(char m[MAZE_SIZE][MAZE_SIZE]) {
   for (int i = 0; i < MAZE_SIZE; ++i) {
     for (int j = 0; j < MAZE_SIZE; ++j) {
@@ -19,31 +17,39 @@ void print_mat(char m[MAZE_SIZE][MAZE_SIZE]) {
 void init(char m[MAZE_SIZE][MAZE_SIZE]) {
   for (int i = 0; i < MAZE_SIZE; ++i) {
     for (int j = 0; j < MAZE_SIZE; ++j) {
-      maze[i][j] = ((j % 2 == 0) || (i % 2 == 0)) ? '#' : '.';
+      m[i][j] = ((j % 2 == 0) || (i % 2 == 0)) ? '#' : '.';
     }
   }
 }
 
-int dfs(int row, int col, char visited[SIZE][SIZE]) {
+int dfs(int row, int col, char visited[SIZE][SIZE],
+        char m[MAZE_SIZE][MAZE_SIZE]) {
   if (visited[row][col] == 1 || row < 0 || row >= SIZE || col < 0 ||
       col >= SIZE) {
     return 0;
   }
 
   visited[row][col] = 1;
-  int r = rand() % 4;
+  int seq[] = {0, 1, 2, 3};
   for (int i = 0; i < 4; ++i) {
-    if ((r + i) % 4 == 0 && dfs(row + 1, col, visited) == 1) {
-      maze[2 * row + 2][2 * col + 1] = '.';
+    int tmp = seq[i];
+    int randi = rand() % 4;
+    seq[i] = seq[randi];
+    seq[randi] = tmp;
+  }
+
+  for (int i = 0; i < 4; ++i) {
+    if (seq[i] == 0 && dfs(row + 1, col, visited, m) == 1) {
+      m[2 * row + 2][2 * col + 1] = '.';
     }
-    if ((r + i) % 4 == 1 && dfs(row, col + 1, visited)) {
-      maze[2 * row + 1][2 * col + 2] = '.';
+    if (seq[i] == 1 && dfs(row, col + 1, visited, m)) {
+      m[2 * row + 1][2 * col + 2] = '.';
     }
-    if ((r + i) % 4 == 2 && dfs(row - 1, col, visited)) {
-      maze[2 * row][2 * col + 1] = '.';
+    if (seq[i] == 2 && dfs(row - 1, col, visited, m)) {
+      m[2 * row][2 * col + 1] = '.';
     }
-    if ((r + i) % 4 == 3 && dfs(row, col - 1, visited)) {
-      maze[2 * row + 1][2 * col] = '.';
+    if (seq[i] == 3 && dfs(row, col - 1, visited, m)) {
+      m[2 * row + 1][2 * col] = '.';
     }
   }
   return 1;
@@ -57,13 +63,15 @@ void create_maze(char m[MAZE_SIZE][MAZE_SIZE]) {
     }
   }
 
-  dfs(0, 0, visited);
+  dfs(0, 0, visited, m);
 
   return;
 }
 
 int main() {
   srand(time(NULL));
+  char maze[MAZE_SIZE][MAZE_SIZE];
+
   init(maze);
   create_maze(maze);
   print_mat(maze);
